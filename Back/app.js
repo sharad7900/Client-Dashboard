ca6a4cefbf52fe0f0146a1eb4cb56698f354ecc0
Sharad Bansal <51768458+sharad7900@users.noreply.github.com>
2025-06-04 00:15:31 +0530

    Update app.js

--- Back/app.js
+++ Back/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = require('./routers/router.js');
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const session = require('express-session');
 const app = express();
 app.use(express.json());
 
@@ -15,6 +16,16 @@ app.use(cookieParser());
 app.use(cors(corsOption));
 app.use("/",router);
 
+app.use(session({
+  secret: 'your-secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None' 
+  }
+}));
 
 app.listen(5000,()=>{
     console.log("Runnig");
