b02ef78e749ba311936ddd6a0e96a945a5265353
sharad7900 <sharadbansal67@gmail.com>
2025-06-17 22:13:10 +0530

    Font update chat

--- Front/src/components/Comments.jsx
+++ Front/src/components/Comments.jsx
@@ -75,7 +75,7 @@ const ChatPage =  () => {
       </Box>
       <HStack>
         <Input
-          style={{width:"100%", color:"black"}}
+          style={{width:"100%"}}
           placeholder="Type a message..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
